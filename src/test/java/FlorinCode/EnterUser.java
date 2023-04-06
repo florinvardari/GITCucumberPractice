087a0f3bdacd28f70ba1c712966dd2e2097cbfb7
florinvardari <florin.vardari@gmail.com>
2023-04-05 21:53:44 -0400

    added extra code

--- src/test/java/FlorinCode/EnterUser.java
+++ src/test/java/FlorinCode/EnterUser.java
@@ -3,6 +3,7 @@ package FlorinCode;
 import io.cucumber.java.en.When;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 
 public class EnterUser {
     WebDriver driver;
@@ -10,5 +11,11 @@ public class EnterUser {
     public void user_enters_valid_email_and_valid_password() {
         driver.findElement(By.id("txtUsername")).sendKeys("admin");
         driver.findElement(By.id("txtPassword")).sendKeys("Hum@nhrm123");
+    }
+    @When("click on login button")
+    public void click_on_login_button() {
+
+        WebElement loginBtn=driver.findElement(By.id("btnLogin"));
+
     }
 }
